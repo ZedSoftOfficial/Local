9a71b5b7f332f4d00e3e778f8446dffcfdfe53e1
ZedSoftOfficial <132583827+ZedSoftOfficial@users.noreply.github.com>
2024-08-23 15:31:24 +0330

    Update 6to4.sh

--- 6to4.sh
+++ 6to4.sh
@@ -17,15 +17,13 @@ setup_rc_local() {
     FILE="/etc/rc.local"
     commands="$1"
 
-    # Ensure the file exists and is executable, or empty it if it already exists
-    if [ -f "$FILE" ]; then
-        sudo bash -c "echo -e '#! /bin/bash\n\nexit 0' > $FILE"
-    else
+    # Ensure the file exists and is executable, or create and set it
+    if [ ! -f "$FILE" ]; then
         echo -e '#! /bin/bash\n\nexit 0' | sudo tee "$FILE" > /dev/null
     fi
     sudo chmod +x "$FILE"
 
-    # Add new commands above 'exit 0'
+    # Add new commands before 'exit 0'
     sudo bash -c "sed -i '/exit 0/i $commands' $FILE"
     echo "Commands added to /etc/rc.local"
 
@@ -34,167 +32,42 @@ setup_rc_local() {
     echo "Commands executed immediately."
 }
 
-# Function to handle Fix Whatsapp Time option
-fix_whatsapp_time() {
-    commands="sudo timedatectl set-timezone Asia/Tehran"
-    setup_rc_local "$commands"
-    echo "Whatsapp time fixed to Asia/Tehran timezone."
-}
+# Function to handle 6to4 multi server (1 outside 2 iran)
+handle_six_to_four_multi_outside_iran() {
+    read -p "Enter the IP outside1: " ipkharej1
+    read -p "Enter the IP outside2: " ipkharej2
+    read -p "Enter the IP Iran: " ipiran
+    read -p "Enter the ports to tunnel for IP outside1 (example: 80,9090): " ports_outside1
+    read -p "Enter the ports to tunnel for IP outside2 (example: 80,9090): " ports_outside2
 
-# Function to handle Optimize option
-optimize() {
-    USER_CONF="/etc/systemd/user.conf"
-    SYSTEM_CONF="/etc/systemd/system.conf"
-    LIMITS_CONF="/etc/security/limits.conf"
-    SYSCTL_CONF="/etc/sysctl.d/local.conf"
-    TEMP_USER_CONF=$(mktemp)
-    TEMP_SYSTEM_CONF=$(mktemp)
+    # Convert comma-separated ports into individual rules
+    IFS=',' read -r -a ports_outside1_array <<< "$ports_outside1"
+    IFS=',' read -r -a ports_outside2_array <<< "$ports_outside2"
 
-    # Function to add line if not exists
-    add_line_if_not_exists() {
-        local file="$1"
-        local line="$2"
-        local temp_file="$3"
+    commands=""
 
-        if [ -f "$file" ]; then
-            cp "$file" "$temp_file"
-            if ! grep -q "$line" "$file"; then
-                sed -i '/^\[Manager\]/a '"$line" "$temp_file"
-                sudo mv "$temp_file" "$file"
-                echo "Added '$line' to $file"
-            else
-                echo "The line '$line' already exists in $file"
-                rm "$temp_file"
-            fi
-        else
-            echo "$file does not exist."
-            rm "$temp_file"
-        fi
-    }
-
-    # Optimize user.conf
-    add_line_if_not_exists "$USER_CONF" "DefaultLimitNOFILE=1024000" "$TEMP_USER_CONF"
-
-    # Optimize system.conf
-    add_line_if_not_exists "$SYSTEM_CONF" "DefaultLimitNOFILE=1024000" "$TEMP_SYSTEM_CONF"
-
-    # Optimize limits.conf
-    if [ -f "$LIMITS_CONF" ]; then
-        cat <<EOF | sudo tee -a "$LIMITS_CONF"
-* hard nofile 1024000
-* soft nofile 1024000
-root hard nofile 1024000
-root soft nofile 1024000
-EOF
-        echo "Added limits to $LIMITS_CONF"
-    else
-        echo "$LIMITS_CONF does not exist."
-    fi
-
-    # Optimize sysctl.d/local.conf
-    cat <<EOF | sudo tee "$SYSCTL_CONF"
-# max open files
-fs.file-max = 1024000
-EOF
-    echo "Added sysctl settings to $SYSCTL_CONF"
-
-    # Apply sysctl changes
-    sudo sysctl --system
-    echo "Sysctl changes applied."
-}
-
-# Function to install x-ui
-install_x_ui() {
-    echo "Choose the version of x-ui to install:"
-    echo "1) alireza"
-    echo "2) MHSanaei"
-    read -p "Select an option (1 or 2): " xui_choice
-
-    if [ "$xui_choice" -eq 1 ]; then
-        bash <(curl -Ls https://raw.githubusercontent.com/alireza0/x-ui/master/install.sh)
-        echo "alireza version of x-ui installed."
-    elif [ "$xui_choice" -eq 2 ]; then
-        bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
-        echo "MHSanaei version of x-ui installed."
-    else
-        echo "Invalid option. Please select 1 or 2."
-    fi
-}
-
-# Function to disable IPv6
-disable_ipv6() {
-    commands=$(cat <<EOF
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+    for port in "${ports_outside1_array[@]}"; do
+        commands+=$(cat <<EOF
+iptables -t nat -A PREROUTING -p tcp -d $ipkharej1 --dport $port -j DNAT --to-destination 10.10.10.1
 EOF
 )
+    done
 
-    setup_rc_local "$commands"
-    echo "IPv6 has been disabled."
-}
-
-# Function to handle 6to4 option
-handle_six_to_four() {
-    echo "Choose the type of server:"
-    echo "1) Outside"
-    echo "2) Iran"
-    read -p "Select an option (1 or 2): " six_to_four_choice
-
-    if [ "$six_to_four_choice" -eq 1 ]; then
-        read -p "Enter the IP outside: " ipkharej
-        read -p "Enter the IP Iran: " ipiran
-
-        commands=$(cat <<EOF
-ip tunnel add 6to4_To_IR mode sit remote $ipiran local $ipkharej
-ip -6 addr add 2002:480:1f10:e1f::2/64 dev 6to4_To_IR
-ip link set 6to4_To_IR mtu 1480
-ip link set 6to4_To_IR up
-
-ip -6 tunnel add GRE6Tun_To_IR mode ip6gre remote 2002:480:1f10:e1f::1 local 2002:480:1f10:e1f::2
-ip addr add 10.10.10.2/30 dev GRE6Tun_To_IR
-ip link set GRE6Tun_To_IR mtu 1436
-ip link set GRE6Tun_To_IR up
+    for port in "${ports_outside2_array[@]}"; do
+        commands+=$(cat <<EOF
+iptables -t nat -A PREROUTING -p tcp -d $ipkharej2 --dport $port -j DNAT --to-destination 10.10.10.2
 EOF
 )
+    done
 
-        setup_rc_local "$commands"
-        echo "Commands executed for the outside server."
-
-    elif [ "$six_to_four_choice" -eq 2 ]; then
-        read -p "Enter the IP Iran: " ipiran
-        read -p "Enter the IP outside: " ipkharej
-
-        commands=$(cat <<EOF
-ip tunnel add 6to4_To_KH mode sit remote $ipkharej local $ipiran
-ip -6 addr add 2002:480:1f10:e1f::1/64 dev 6to4_To_KH
-ip link set 6to4_To_KH mtu 1480
-ip link set 6to4_To_KH up
-
-ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote 2002:480:1f10:e1f::2 local 2002:480:1f10:e1f::1
-ip addr add 10.10.10.1/30 dev GRE6Tun_To_KH
-ip link set GRE6Tun_To_KH mtu 1436
-ip link set GRE6Tun_To_KH up
-
+    commands+=$(cat <<EOF
 sysctl net.ipv4.ip_forward=1
-EOF
-)
-
-        # Add iptables rules to rc.local for Iran server
-        iptables_rules=$(cat <<EOF
-iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination 10.10.10.1
-iptables -t nat -A PREROUTING -j DNAT --to-destination 10.10.10.2
 iptables -t nat -A POSTROUTING -j MASQUERADE
 EOF
 )
 
-        setup_rc_local "$commands"
-        setup_rc_local "$iptables_rules"
-        echo "Commands executed for the Iran server."
-
-    else
-        echo "Invalid option. Please select 1 or 2."
-    fi
+    setup_rc_local "$commands"
+    echo "Commands executed for the multi-server (1 Outside, 2 Iran) setup."
 }
 
 # Function to handle 6to4 multi server (1 iran 2 outside)
@@ -244,44 +117,6 @@ EOF
     echo "Commands executed for the multi-server (1 Iran, 2 Outside) setup."
 }
 
-# Function to handle 6to4 multi server (1 outside 2 iran)
-handle_six_to_four_multi_outside_iran() {
-    read -p "Enter the IP outside1: " ipkharej1
-    read -p "Enter the IP outside2: " ipkharej2
-    read -p "Enter the IP Iran: " ipiran
-    read -p "Enter the ports to tunnel for IP outside1 (example: 80,9090): " ports_outside1
-    read -p "Enter the ports to tunnel for IP outside2 (example: 80,9090): " ports_outside2
-
-    # Convert comma-separated ports into individual rules
-    IFS=',' read -r -a ports_outside1_array <<< "$ports_outside1"
-    IFS=',' read -r -a ports_outside2_array <<< "$ports_outside2"
-
-    commands=""
-
-    for port in "${ports_outside1_array[@]}"; do
-        commands+=$(cat <<EOF
-iptables -t nat -A PREROUTING -p tcp -d $ipkharej1 --dport $port -j DNAT --to-destination 10.10.10.1
-EOF
-)
-    done
-
-    for port in "${ports_outside2_array[@]}"; do
-        commands+=$(cat <<EOF
-iptables -t nat -A PREROUTING -p tcp -d $ipkharej2 --dport $port -j DNAT --to-destination 10.10.10.2
-EOF
-)
-    done
-
-    commands+=$(cat <<EOF
-sysctl net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -j MASQUERADE
-EOF
-)
-
-    setup_rc_local "$commands"
-    echo "Commands executed for the multi-server (1 Outside, 2 Iran) setup."
-}
-
 # Function to remove tunnels
 remove_tunnels() {
     echo "Removing tunnels..."
@@ -301,6 +136,41 @@ remove_tunnels() {
     echo "Tunnels removed and /etc/rc.local updated."
 }
 
+# Function to fix Whatsapp time
+fix_whatsapp_time() {
+    commands="sudo timedatectl set-timezone Asia/Tehran"
+    setup_rc_local "$commands"
+    echo "Whatsapp time fixed to Asia/Tehran timezone."
+}
+
+# Function to install x-ui
+install_x_ui() {
+    echo "Choose the version of x-ui to install:"
+    echo "1) alireza"
+    echo "2) MHSanaei"
+    read -p "Select an option (1 or 2): " xui_choice
+
+    if [ "$xui_choice" -eq 1 ]; then
+        bash <(curl -Ls https://raw.githubusercontent.com/alireza0/x-ui/master/install.sh)
+        echo "alireza version of x-ui installed."
+    elif [ "$xui_choice" -eq 2 ]; then
+        bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+        echo "MHSanaei version of x-ui installed."
+    else
+        echo "Invalid option. Please select 1 or 2."
+    fi
+}
+
+# Function to enable BBR
+enable_bbr() {
+    commands=$(cat <<EOF
+sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+EOF
+)
+    setup_rc_local "$commands"
+    echo "BBR enabled."
+}
+
 # Function to change nameserver
 change_nameserver() {
     read -p "Enter the new nameserver IP address: " nameserver
@@ -308,10 +178,33 @@ change_nameserver() {
     echo "Nameserver changed to $nameserver."
 }
 
+# Function to disable IPv6
+disable_ipv6() {
+    commands=$(cat <<EOF
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+EOF
+)
+    setup_rc_local "$commands"
+    echo "IPv6 disabled."
+}
+
+# Function to optimize settings
+optimize() {
+    commands=$(cat <<EOF
+sudo sysctl -w net.core.somaxconn=1024
+sudo sysctl -w net.ipv4.tcp_max_syn_backlog=2048
+EOF
+)
+    setup_rc_local "$commands"
+    echo "System optimized."
+}
+
 # Execute the selected option
 case $server_choice in
     1)
-        handle_six_to_four
+        echo "Handling 6to4..."
         ;;
     2)
         handle_six_to_four_multi_iran_kharej
@@ -323,16 +216,16 @@ case $server_choice in
         remove_tunnels
         ;;
     5)
-        optimize
+        enable_bbr
         ;;
     6)
         fix_whatsapp_time
         ;;
     7)
-        install_x_ui
+        optimize
         ;;
     8)
-        disable_ipv6
+        install_x_ui
         ;;
     9)
         change_nameserver
